29fbb14392211fe4b80bef8191e02b6ff75eff64
aokinatsuki <76143435+aokinatsuki@users.noreply.github.com>
2024-12-24 15:43:01 +0900

    jqueryのモジュールとして使えるように改修
    ・最新が上に来るように修正
    ・同じ位置にポップアップが出る問題を修正
    ・×ボタン押下で位置が繰り下がるように修正

--- noticePopup.js
+++ noticePopup.js
@@ -1,154 +1,187 @@
-// **各classについて**
+/**
+ * jQuery Plugin: noticePopup - カスタム通知ポップアップ
+ *
+ * このプラグインを使用すると、簡単に通知ポップアップを作成できます。
+ * サーバーサイドの監視と手動で通知を追加する機能をサポートします。
+ *
+ * 使用例:
+ *
+ * 1. サーバー通知を自動的に監視する:
+ *    $.noticePopup({
+ *        isServerSide: true,
+ *        url: '/api/notifications',
+ *        color: 'green',
+ *        interval: 30000 // 30秒ごとにサーバーを監視
+ *    });
+ *
+ * 2. 手動で通知を追加する:
+ *    $.noticePopup('showNotice', {
+ *        color: 'blue'
+ *    }, 0, 'タイトル', '手動で追加された通知メッセージです。');
+ *
+ * 3. すべての通知を削除する:
+ *    $.noticePopup('destroy');
+ *
+ * オプション:
+ * - isServerSide: サーバー通知を有効にするかどうか (デフォルト: false)
+ * - url: サーバー通知を取得するURL (必須: isServerSide = trueの場合)
+ * - func: カスタムコールバック関数
+ * - id: ポップアップのユニークなID (デフォルト: 'notice_popup')
+ * - color: ポップアップの色を指定 ('default', 'green', 'blue' など)
+ * - size: ポップアップのサイズ ('sm', 'md', 'lg' など)
+ * - titleShow: タイトルを表示するかどうか (デフォルト: true)
+ * - msgShow: メッセージを表示するかどうか (デフォルト: true)
+ * - interval: サーバー監視の間隔 (ミリ秒, デフォルト: 60000)
+ */
 
-
-// **独自イベントについて**
-// 表示時: noticeShowBefore, noticeShowAfter
-// メッセージ内容追加イベント: noticeMsgText
-
-// **外から呼び出せるメソッドについて**
-// const 変数 = new noticePopup();
-// 変数.init() のように呼び出せます
-// 初期設定: init() option{isServerSide: ポップアップの判別をajax経由にするか(Bool), url: isServerSideがtrueの場合はurl指定, func: isServerSideがfalseの場合のチェック処理 ()=>{処理}, id: ポップアップ自体につける独自id, color: 色指定, size: sm・md・lg（ポップアップのサイズ指定）, titleShow: タイトルエリア表示(Bool), msgShow: メッセージエリア表示(Bool)}
-// 破壊用: destroy() ※必ず更新するnodeを渡す※
-
-const noticePopup = function(){
-    // デフォルトのオプション設定
-    const defaultOption = {
-        isServerSide: false,
-        url: "",
-        func: ()=>{},
-        id: 'notice_popup',
-        color: "default",
-        size: "md",
-        titleShow: true,
-        msgShow: true
+(function($) {
+    const defaults = {
+        isServerSide: false, // サーバー監視を有効にするか
+        url: "", // サーバー通知を取得するURL
+        func: () => {}, // カスタム処理
+        id: 'notice_popup', // ポップアップID
+        color: "default", // ポップアップの色
+        size: "md", // ポップアップのサイズ
+        titleShow: true, // タイトルを表示するか
+        msgShow: true, // メッセージを表示するか
+        interval: 60000 // デフォルトの監視間隔 (ミリ秒)
     };
 
-    let setOptionlist = {};
+    const popupState = {
+        popupCount: 0, // 現在のポップアップ数を追跡
+    };
 
-    // カスタム色設定可能なもの
-    /**
-     * red
-     * green
-     * default
-     */
+    const methods = {
+        /**
+         * 初期化処理
+         * @param {Object} options - ユーザー設定
+         */
+        init: function(options) {
+            const settings = $.extend({}, defaults, options);
+            if (settings.isServerSide && (!settings.url || settings.url.length === 0)) {
+                console.error("Error[noticePopup]: isServerSide is true but url is null");
+                return;
+            }
 
-    // 初期設定
-    noticePopup.prototype.init = function(options={}){
-        let setOption = {};
-        $.each(Object.keys(defaultOption), function(idx, val){
-            // 独自で設定があればその設定をセットする
-            setOption[val] = val in options ? options[val] : defaultOption[val];
-        });
-        setOptionlist = setOption;
+            if (settings.isServerSide) {
+                setInterval(() => {
+                    methods._checkServerNotifications(settings);
+                }, settings.interval);
+            }
 
-        // サーバーサイド指定だが、urlなしのためエラーを返す
-        if(setOptionlist.isServerSide && (setOptionlist.url.length === 0 || setOptionlist.url === undefined)){
-            return error_log("Error[noticePopup]: isServerSide is true but url is null");
-        }
+            return this;
+        },
 
-        init(setOption);
-    }
-    // 破壊
-    noticePopup.prototype.destroy = function(elm=null){
-
-    }
-
-    function init(options){
-        monitor(options);
-        // 10秒ごとに監視
-        setInterval(function(){
-            console.log("10秒経ちました");
-            monitor(options);
-        },10000);
-    }
-
-    // データ監視ajax処理
-    function monitor(options){
-        let notices = "";
-        if(options.isServerSide){
-            let res = $.ajax({
+        /**
+         * サーバー通知を取得する
+         * @param {Object} settings - 現在の設定
+         */
+        _checkServerNotifications: function(settings) {
+            $.ajax({
                 type: 'GET',
-                url: options.url,
-                async: false,
-                datatype: "json"
-            }).responseText;
-            notices = $.parseJSON(res);
+                url: settings.url,
+                dataType: "json",
+                success: function(res) {
+                    if (res && res.length > 0) {
+                        $.each(res, function(idx, val) {
+                            methods.showNotice(settings, idx, val.title, val.message);
+                        });
+                    }
+                },
+                error: function(err) {
+                    console.error("Error[noticePopup]: Ajax request failed", err);
+                }
+            });
+        },
+
+        /**
+         * 通知を表示する
+         * @param {Object} settings - 現在の設定
+         * @param {number} idx - 通知のインデックス
+         * @param {string} title - 通知のタイトル
+         * @param {string} msg - 通知のメッセージ
+         */
+        showNotice: function(settings, idx = 0, title = "", msg = "") {
+            methods._createPopup(settings, popupState.popupCount, title, msg);
+            $(`#${settings.id}_${popupState.popupCount}`).fadeIn(500).trigger("noticeMsgText");
+            methods._adjustPopupPosition(settings);
+            popupState.popupCount++;
+        },
+
+        /**
+         * HTMLを生成する
+         * @param {Object} settings - 現在の設定
+         * @param {number} idx - 通知のインデックス
+         * @param {string} title - 通知のタイトル
+         * @param {string} msg - 通知のメッセージ
+         */
+        _createPopup: function(settings, idx = 0, title = "", msg = "") {
+            if ($(`#${settings.id}_${idx}`).length === 0) {
+                const html = `
+                    <div class="notice_popup" id="${settings.id}_${idx}" data-prefix="${settings.id}">
+                        <span class="batsu"></span>
+                        <div class="notice_popup_content color-${settings.color} py-3">
+                            ${settings.titleShow ? `<div class="title">${title}</div>` : ''}
+                            ${settings.msgShow ? `<div class="message">${msg}</div>` : ''}
+                        </div>
+                    </div>`;
+                $(html).prependTo("body");
+            }
+        },
+
+        /**
+         * ポップアップの位置を調整する
+         * @param {Object} settings - 現在の設定
+         */
+        _adjustPopupPosition: function(settings) {
+            const popupHeight = $(".notice_popup").outerHeight();
+            const bottomOffset = 1.5 * popupHeight * popupState.popupCount;
+            $(`#${settings.id}_${popupState.popupCount - 1}`).css('bottom', `${bottomOffset}px`);
+        },
+
+        /**
+         * 全てのポップアップを削除する
+         */
+        destroy: function() {
+            $(".notice_popup").remove();
+            popupState.popupCount = 0;
+        }
+    };
+
+    // jQueryプラグインとして登録
+    $.noticePopup = function(methodOrOptions) {
+        if (methods[methodOrOptions]) {
+            return methods[methodOrOptions].apply(this, Array.prototype.slice.call(arguments, 1));
+        } else if (typeof methodOrOptions === 'object' || !methodOrOptions) {
+            return methods.init.apply(this, arguments);
         } else {
-            notices = options.func;
+            $.error(`Method ${methodOrOptions} does not exist on jQuery.noticePopup`);
         }
+    };
 
-        if(notices.length > 0){
-            $('.notice_popup').trigger('noticeShowBefore');
+    // 動的要素にイベントをバインド
+    $(document).on("click", ".batsu", function() {
+        const popup = $(this).parent(".notice_popup");
 
-            $.each(notices, function(idx, val){
-                show_notice(val.title, val.message, idx);
-            })
+        // プレフィックスidを取得
+        const popupId = popup.attr('data-prefix');
 
-            $('.notice_popup').trigger('noticeShowAfter');
-        }
-    }
+        popup.fadeOut(500, function() {
+            $(this).remove();
 
-    // 通知情報表示処理
-    function show_notice(title="", msg="", idx=0){
-        init_create_html(idx, title, msg);
+            popupState.popupCount--;
 
-        $(`#${setOptionlist.id}_${idx}`).trigger("show");
-
-        $(`#${setOptionlist.id}_${idx}`).trigger('noticeMsgText');
-
-        setTimeout(function(){
-            $(`#${setOptionlist.id}_${idx}`).trigger("hide");
-        }, 8000);
-    }
-
-    // 通知の吹き出しのhtml生成(初回に呼び出す処理)
-    function init_create_html(idx=0, title="", msg=""){
-        // まだ生成していない場合はhtml生成
-        if($(`#${setOptionlist.id}_${idx}`).length === 0){
-            let html = `<div class="notice_popup" id="${setOptionlist.id}_${idx}">`;
-            html += '<span class="batsu"></span>';
-            html += `<div class="notice_popup_content color-${setOptionlist.color} mb-5 py-3">`;
-            if(setOptionlist.titleShow){
-                html += '<div class="title">'+ title +'</div>';
+            let last = null;
+            // 残りのポップアップ位置を再調整
+            $(".notice_popup").each(function(index) {
+                last = $(this);
+                // id振り直し
+                $(this).attr("id", `${popupId}_${index}`);
+                $(this).css('bottom', `${1.5 * $(this).outerHeight() * (index + 1)}px`);
+            });
+            if(last !== null){
+                last.css('bottom', "1rem");
             }
-            if(setOptionlist.msgShow){
-                html += '<div class="message">'+ msg +'</div>';
-            }
-            html += '</div>';
-            html += '</div>';
-            $(html).appendTo("#gen_maincontent");
-        }
-    }
-
-    // ×ボタン押下
-    $(document).on("click", ".batsu", function(){
-        $(this).parent(".notice_popup").trigger("hide");
-    })
-
-    // 表示処理
-    $(document).on("show", ".notice_popup", function(){
-        $(this).fadeIn(500);
-    })
-
-    // 非表示処理
-    $(document).on("hide", ".notice_popup", function(){
-        $(this).fadeOut(500);
-    })
-
-    // エラーログ用
-    function error_log(msg=''){
-        // 指定のメッセージがある場合はそれをログに出す
-        if(msg.length > 0){
-            console.error(msg);
-        }
-
-        // 破壊の時に指定のnodeが渡されていない場合はエラー
-        if(isDeleteFlg){
-            isDeleteFlg = false;
-            console.error('Error[noticePopup]: not select destroy node');
-        }
-
-        return;
-    }
-}
+        });
+    });
+})(jQuery);
